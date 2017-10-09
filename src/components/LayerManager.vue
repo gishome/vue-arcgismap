087df50943e15bb6d5d8467a1c05c04468224d54
332383124 <332383124@qq.com>
2017-10-09 09:13:55 +0800

    update

---
+++ src/components/LayerManager.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container" v-bind:class="{ active: !searchPaneActive}" @dragover.prevent :draggable="true" @dragstart.stop="handleDragStart" @drop.stop="handleDrop" @dragenter.stop="handleDragEnter" @dragleave.stop="handleDragLeave" @dragend.prevent="handleDragEnd">
+    <Layer v-for="(item, index) in layers" v-bind:key="item.id" v-bind:item="item" />
+
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+import Layer from './LayerManager/Layer'
+
+export default {
+
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      searchPaneActive: true
+    }
+  }, components: {
+    Layer
+  }, methods: {
+    _swtich(value) {
+      // this.$store.dispatch({ type: 'searchPane', switchpane: true });
+    },
+    handleDragStart: function(evt, a, b, c) {
+      console.log(evt);
+    },
+    handleDrop: function(evt) {
+      console.log(evt);
+    },
+    handleDragEnter: function(evt) {
+      console.log(evt);
+
+    },
+    handleDragLeave: function(evt) {
+      console.log(evt);
+    },
+    handleDragEnd: function(evt) {
+      console.log(evt);
+    }
+  }, computed: mapGetters({
+    layers: 'getAllLayers'
+  }),
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 100%;
+  background: rebeccapurple;
+}
+
+
+
+.layers .item {
+  background: red
+}
+</style>
---
+++ src/components/LayerManager/Layer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-bind:class="{ active: mouseover}" class="layer" @mouseover="mouseOver($event,true)">
+
+    <div class="title">title:{{item.title||'--'}}</div>
+    <div class="opacity">opacity:{{item.opacity}}</div>
+    <div class="drag">拖动</div>
+    <div class="display">显示</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'item'
+  ],
+  methods: {
+    mouseOver: function(evt, param) {
+      this.mouseover = param;
+      evt.stopPropagation();
+    }
+  },
+
+  name: 'layer',
+  data() {
+    return {
+      mouseover: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.layer {
+  background: red
+}
+
+.layer:hover {
+  background: green !important;
+}
+</style>
